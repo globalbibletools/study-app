9d80244a140e9e393aa4efae8d3737d8903e35cb
ethnosdev <contact@ethnos.dev>
2025-06-17 15:57:31 +0800

    cleanup

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'package:database_builder/database_builder.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:studyapp/home/hebrew_greek_text.dart';
@@ -18,9 +17,6 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final manager = HomeManager();
   final _scrollController = ScrollController();
-  // OverlayEntry? _overlayEntry;
-  // Timer? _glossTimer;
-  // List<GlobalKey> _wordKeys = [];
 
   // Pinch to zoom font scaling
   static const double _baseFontSize = 20.0;
@@ -44,93 +40,9 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   void dispose() {
-    // _removeGlossOverlay();
     _scrollController.dispose();
     super.dispose();
   }
-
-  // void _showGlossOverlay(String word, GlobalKey key) {
-  //   _removeGlossOverlay();
-
-  //   final renderBox = key.currentContext?.findRenderObject() as RenderBox?;
-  //   if (renderBox == null) return;
-
-  //   final size = renderBox.size;
-  //   final position = renderBox.localToGlobal(Offset.zero);
-  //   final fontSize = _baseFontSize * _currentScale * 0.8;
-
-  //   final theme = Theme.of(context);
-
-  //   // Measure the width of the popup text
-  //   final textSpan = TextSpan(
-  //     text: word,
-  //     style: theme.textTheme.bodyMedium!.copyWith(fontSize: fontSize),
-  //   );
-  //   final textPainter = TextPainter(
-  //     text: textSpan,
-  //     textDirection: TextDirection.rtl,
-  //   );
-  //   textPainter.layout();
-
-  //   const horizontalPadding = 8.0;
-  //   const verticalPadding = 4.0;
-  //   final popupWidth = textPainter.width + (horizontalPadding * 2);
-  //   final screenSize = MediaQuery.sizeOf(context);
-  //   double left = position.dx + size.width / 2 - popupWidth / 2;
-  //   final verticalOffset = max(30, 30 * _currentScale);
-  //   final top = position.dy - verticalOffset;
-
-  //   // Adjust if going off screen
-  //   const edgePadding = 8.0;
-  //   if (left < 0) {
-  //     left = edgePadding;
-  //   } else if (left + popupWidth > screenSize.width) {
-  //     left = screenSize.width - popupWidth - edgePadding;
-  //   }
-
-  //   _overlayEntry = OverlayEntry(
-  //     builder:
-  //         (context) => Positioned(
-  //           top: top,
-  //           left: left,
-  //           child: Container(
-  //             padding: const EdgeInsets.symmetric(
-  //               horizontal: horizontalPadding,
-  //               vertical: verticalPadding,
-  //             ),
-  //             decoration: BoxDecoration(
-  //               color: theme.textTheme.bodyMedium!.color,
-  //               borderRadius: BorderRadius.circular(4),
-  //               boxShadow: [
-  //                 BoxShadow(
-  //                   color: Colors.black,
-  //                   blurRadius: 4.0,
-  //                   offset: Offset(0, 2),
-  //                 ),
-  //               ],
-  //             ),
-  //             child: Text(
-  //               word,
-  //               style: theme.textTheme.bodyMedium!.copyWith(
-  //                 color: Colors.black87,
-  //                 fontSize: fontSize,
-  //               ),
-  //             ),
-  //           ),
-  //         ),
-  //   );
-  //   Overlay.of(context).insert(_overlayEntry!);
-
-  //   _glossTimer = Timer(const Duration(milliseconds: 3000), () {
-  //     _removeGlossOverlay();
-  //   });
-  // }
-
-  // void _removeGlossOverlay() {
-  //   _glossTimer?.cancel();
-  //   _overlayEntry?.remove();
-  //   _overlayEntry = null;
-  // }
 
   @override
   Widget build(BuildContext context) {
@@ -146,7 +58,6 @@ class _HomeScreenState extends State<HomeScreen> {
                 },
               ),
               onPressed: () {
-                // _removeGlossOverlay();
                 _showBookChooserDialog();
               },
             ),
@@ -159,7 +70,6 @@ class _HomeScreenState extends State<HomeScreen> {
                 },
               ),
               onPressed: () {
-                // _removeGlossOverlay();
                 manager.showChapterChooser();
               },
             ),
@@ -279,38 +189,6 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
     );
   }
-
-  // static const maqaph = '־';
-
-  // List<Widget> _createTextWidgets(List<HebrewGreekWord> words) {
-  //   final textWidgets = List.generate(words.length, (index) {
-  //     final word = words[index];
-  //     final verse = _extractVerse(word);
-  //     final space = (word.text.endsWith(maqaph)) ? '' : ' ';
-  //     final wordText =
-  //         (verse == null) ? '${word.text}$space' : '$verse ${word.text}$space';
-  //     final key = _wordKeys[index];
-  //     return GestureDetector(
-  //       key: key,
-  //       onTap: () async {
-  //         final gloss = await manager.lookupGlossForId(word.id);
-  //         _showGlossOverlay(gloss, key);
-  //       },
-  //       child: Text(
-  //         wordText,
-  //         textDirection:
-  //             manager.currentChapterIsRtl
-  //                 ? TextDirection.rtl
-  //                 : TextDirection.ltr,
-  //         style: TextStyle(
-  //           fontFamily: 'sbl',
-  //           fontSize: _baseFontSize * _currentScale,
-  //         ),
-  //       ),
-  //     );
-  //   });
-  //   return textWidgets;
-  // }
 
   Future<void> _showBookChooserDialog() async {
     manager.chapterCountNotifier.value = null;
