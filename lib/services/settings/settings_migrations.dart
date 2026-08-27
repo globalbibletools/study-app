1e9d140bc4031d51843884cab0dd1e9bef45582e
Adrian Rocke <me@adrianrocke.com>
2026-08-26 21:28:32 -0500

    settings migration to for bible and gloss lang

--- lib/services/settings/settings_migrations.dart
+++ lib/services/settings/settings_migrations.dart
@@ -1,3 +1,18 @@
+/*
+SETTINGS MIGRATIONS
+
+Settings migrations allow us to gracefully change the schema of user settings
+with minimal disruptions to the user.
+
+To add a new migration:
+1. Add a new function named migrateV* in descending order at the end of this file.
+2. Add the function to the end of the _settingsMigration list.
+
+When the app is opened, it will run any new migrations that it finds.
+It is important that we do not delete any migrations
+since we don't know how many versions the user might have skipped.
+*/
+
 import 'dart:developer';
 
 import 'package:shared_preferences/shared_preferences.dart';
@@ -21,4 +36,33 @@ Future<void> migrateSettings(SharedPreferences settings) async {
 }
 
 const List<Future<void> Function(SharedPreferences)> _settingsMigrations = [
+    migrateV1,
 ];
+
+Future<void> migrateV1(SharedPreferences settings) async {
+    final locale = settings.getString('locale');
+
+    switch (locale) {
+        case 'en': {
+            await settings.setString("currentBible", "eng_bsb");
+            await settings.setString("glossLang", "eng");
+        }
+        case 'es': {
+            await settings.setString("currentBible", "spa_blm");
+            await settings.setString("glossLang", "spa");
+        }
+        case 'fr': {
+            await settings.setString("currentBible", "fra_lsg");
+            await settings.setString("glossLang", "fra");
+        }
+        case 'pt': {
+            await settings.setString("currentBible", "por_blj");
+            await settings.setString("glossLang", "por");
+        }
+        case 'ar': {
+            await settings.setString("currentBible", "arb_vdv");
+            await settings.setString("glossLang", "are");
+        }
+    }
+}
+
